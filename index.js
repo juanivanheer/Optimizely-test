7872c1091a8331261106ec5fa77f7795c04af00f
juanivanheer <juanivanheer@gmail.com>
2021-08-02 13:10:39 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-// Import stylesheets
-import './style.css';
+const optimizelySdk = require('@optimizely/optimizely-sdk');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const optimizelyClient = optimizelySdk.createInstance({
+  sdkKey: 'GD1YnswD17rAwke3qSy5q'
+});
+
+optimizelyClient.onReady().then(() => {
+  for (let i = 0; i < 1; i++) {
+    let userId = Math.floor(Math.random() * (10000 - 1000) + 1000).toString();
+
+    let user = optimizelyClient.createUserContext(userId);
+    let decision = user.decide('virtual_assistant_routes');
+    console.log(decision);
+
+    console.log(`\nVariation: ${decision.variationKey}`);
+    console.log(`with values: 
+    login = ${decision.variables['login']}
+    accounts_account-overview = ${
+      decision.variables['accounts_account-overview']
+    }
+    enrollment = ${decision.variables['enrollment']}
+    help-and-support_faq = ${decision.variables['help-and-support_faq']}`);
+  }
+});
