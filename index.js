23cd7472bf711d4aa3cd77a95303b6b2e86c4769
juanivanheer <juanivanheer@gmail.com>
2021-08-03 11:04:10 +0200

    2 files modified

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ optimizelyClient.onReady().then(() => {
   for (let i = 0; i < 1; i++) {
     let userId = Math.floor(Math.random() * (10000 - 1000) + 1000).toString();
 
+    console.log(optimizelyClient);
+
     let user = optimizelyClient.createUserContext(userId);
     let decision = user.decide('virtual_assistant_routes');
     console.log(decision);
 
     console.log(`\nVariation: ${decision.variationKey}`);
     console.log(`with values:       
-    login = ${decision.variables['login']}
-    accounts_account-overview = ${ 
+    login = ${decision.variables['login']} 
+    accounts_account-overview = ${
       decision.variables['accounts_account-overview']
     }
     enrollment = ${decision.variables['enrollment']}
     help-and-support_faq = ${decision.variables['help-and-support_faq']}`);
+
+    optimizelyClient.track('test', userId);
+
+    let variation_html = document.getElementById("variation")
+    variation_html.innerHTML = 'VARIATION: ' + decision.variationKey
+
+    document.getElementById("login").innerHTML = 'login: ' + decision.variables['login'];
+
+    document.getElementById("accounts_account-overview").innerHTML = 'accounts_account-overview: ' + 
+      decision.variables['accounts_account-overview']
+
+    document.getElementById("enrollment").innerHTML = 'enrollment: ' + decision.variables['enrollment'];
+
+    document.getElementById("help-and-support_faq").innerHTML = 'help-and-support_faq: ' + decision.variables['help-and-support_faq'];
   }
 });
